b97efcaad5ff86b867522f17226b50b7f108426a
Christian Wacker <christianwwacker@gmail.com>
2020-01-10 19:15:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,3 +25,13 @@ function johnLennonFacts(facts){
   }
   return facts
 }
+
+function iLoveTheBeatles(number){
+  var array = []
+  do {
+    array.push("I love the Beatles!");
+    number++;
+  } while (number < 15);
+  
+  return array;
+}
